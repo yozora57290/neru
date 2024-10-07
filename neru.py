fea6790524190fcc1897847127acc1a5386a8b5a
Yozora57290 <mihiro57290@gmail.com>
2024-10-07 20:04:16 +0900

    feat: ロゴを表示

--- neru.py
+++ neru.py
@@ -3,8 +3,12 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Neru"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    page.add(ft.Text("Hello, world!"))
+    logo = ft.Text("Neru", size=32)
+
+    page.add(ft.Column([logo]))
 
 
 if __name__ == "__main__":
