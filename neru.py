66ab071156a3a82446b398fa11ee6ee05e8a806e
Yozora57290 <mihiro57290@gmail.com>
2024-10-07 20:13:50 +0900

    feat: 記録・管理・設定のボタンを追加

--- neru.py
+++ neru.py
@@ -7,8 +7,18 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     logo = ft.Text("Neru", size=32)
+    record_button = ft.ElevatedButton("睡眠を記録")
+    manage_button = ft.ElevatedButton("睡眠を管理")
+    settings_button = ft.ElevatedButton("設定")
 
-    page.add(ft.Column([logo]))
+    page.add(
+        ft.Column(
+            [logo, record_button, manage_button, settings_button],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=30,
+        )
+    )
 
 
 if __name__ == "__main__":
